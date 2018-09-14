2c1b4c7040f0058bc8d99ded5600bc7a7a6821af
Rahul Anoop Kelkar <42903938+aarkay07@users.noreply.github.com>
2018-09-14 23:45:45 +0530

    Add files via upload

---
+++ wifi_ldr/wifi_ldr.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "aarkay";
+const char* password = "isisisis";
+
+int sensorPin = A0; 
+
+int WiFiCon() {
+    // Check if we have a WiFi connection, if we don't, connect.
+  int xCnt = 0;
+
+  if (WiFi.status() != WL_CONNECTED){
+
+        Serial.println();
+        Serial.println();
+        Serial.print("Connecting to ");
+        Serial.println(ssid);
+
+        WiFi.mode(WIFI_STA);
+        
+        WiFi.begin(ssid, password);
+        
+        while (WiFi.status() != WL_CONNECTED  && xCnt < 50) {
+          delay(500);
+          Serial.print(".");
+          xCnt ++;
+        }
+
+        if (WiFi.status() != WL_CONNECTED){
+          Serial.println("WiFiCon=0");
+          return 0; //never connected
+        } else {
+          Serial.println("WiFiCon=1");
+          Serial.println("");
+          Serial.println("WiFi connected");  
+          Serial.println("IP address: ");
+          Serial.println(WiFi.localIP());
+          return 1; //1 is initial connection
+        }
+
+  } else {
+    Serial.println("WiFiCon=2");
+    return 2; //2 is already connected
+  
+  }
+}
+
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  WiFiCon();
+
+}
+
+void loop() {
+
+  Serial.println(analogRead(sensorPin));
+  //delay(2000);
+  if (WiFiCon() > 0) {
+    //do something
+
+    
+  }
+
+  delay(100);
+
+}
