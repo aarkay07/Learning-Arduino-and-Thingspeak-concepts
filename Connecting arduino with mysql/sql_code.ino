8c96140d7e0aab4620bc9b154494b7f76f128407
Rahul Anoop Kelkar <42903938+aarkay07@users.noreply.github.com>
2018-09-17 11:10:13 +0530

    Add files via upload

---
+++ Connecting arduino with mysql/sql_code.ino
@@ -0,0 +1,56 @@
+#include <mysql_connection.h>
+/*
+#include <mysql.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+*/
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  char host[] = "localhost";
+  char user[] = "root";
+  char pass[] = "";
+  char db[] = "arduino";
+  int isconnected = mysql_connect(host, user, pass, db);
+  if(isconnected)
+  {
+    Serial.print("Connected to ");
+    Serial.println(host);
+  }
+  else
+  {
+    Serial.println("Connection failed. ");
+  }
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  mysql_close();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&isconnected);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  cur_mem->execute("inserti into ultra values distance");
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+  
+}
+
